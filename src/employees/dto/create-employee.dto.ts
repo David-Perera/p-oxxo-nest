ef85c6e42186937b8e5a8cf88894097cb5aa036f
David-Perera <chobito2006@gmail.com>
2025-09-28 01:32:49 -0600

    Integracion CRUD employees

---
+++ src/employees/dto/create-employee.dto.ts
@@ -0,0 +1,5 @@
+export class CreateEmployeeDto {
+    id: number;
+    name: string;
+    position: string;
+}
---
+++ src/employees/employees.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+
+@Injectable()
+export class EmployeesService {
+  private readonly employees: CreateEmployeeDto[] = [
+    { id: 1, name: 'John Doe', position: 'Software Engineer' },
+    { id: 2, name: 'Jane Smith', position: 'Project Manager' },
+    { id: 3, name: 'Alice Johnson', position: 'UX Designer' },
+  ];
+  create(createEmployeeDto: CreateEmployeeDto) {
+    createEmployeeDto
+    this.employees.push(createEmployeeDto);
+    return createEmployeeDto;
+  }
+
+  findAll() {
+    return this.employees;
+  }
+
+  findOne(id: number) {
+    const employee = this.employees.filter((employee) => employee.id === id)[0];
+    return employee;
+  }
+
+  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    return `This action updates a #${id} employee`;
+  }
+
+  remove(id: number) {
+    return this.employees.filter((employee) => employee.id !== id);
+  }
+}
